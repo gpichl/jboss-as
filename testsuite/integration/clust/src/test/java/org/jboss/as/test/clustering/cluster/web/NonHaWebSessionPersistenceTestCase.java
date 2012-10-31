074f3a54c0fb5e227ee1b9fed52d661ab7fcfeb6
Radoslav Husar <radosoft@gmail.com>
2012-10-31 21:38:27 +0100

    AS7-4460 adapt NonHaWebSessionPersistenceTestCase

--- testsuite/integration/clust/src/test/java/org/jboss/as/test/clustering/cluster/web/NonHaWebSessionPersistenceTestCase.java
+++ testsuite/integration/clust/src/test/java/org/jboss/as/test/clustering/cluster/web/NonHaWebSessionPersistenceTestCase.java
@@ -45,11 +45,13 @@ import org.jboss.arquillian.container.test.api.TargetsContainer;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.as.test.clustering.cluster.ClusterAbstractTestCase;
 import org.jboss.as.test.clustering.single.web.SimpleServlet;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -61,7 +63,7 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Arquillian.class)
 @RunAsClient
-public class NonHaWebSessionPersistenceTestCase {
+public class NonHaWebSessionPersistenceTestCase extends ClusterAbstractTestCase {
 
     @Deployment(name = DEPLOYMENT_1, managed = false, testable = false)
     @TargetsContainer(CONTAINER_SINGLE)
@@ -72,16 +74,13 @@ public class NonHaWebSessionPersistenceTestCase {
         return war;
     }
 
-    @ArquillianResource
-    private ContainerController controller;
-    @ArquillianResource
-    private Deployer deployer;
+    @Override
+    public void testSetup() {
+        // TODO rethink how this can be done faster with one less stopping (eg. make this test last)
+        stop(CONTAINER_1);
 
-    @Test
-    @InSequence(-1)
-    public void testStartContainerAndDeployment() {
-        controller.start(CONTAINER_SINGLE);
-        deployer.deploy(DEPLOYMENT_1);
+        start(CONTAINER_SINGLE);
+        deploy(DEPLOYMENT_1);
     }
 
     @Test
@@ -98,8 +97,8 @@ public class NonHaWebSessionPersistenceTestCase {
             Assert.assertFalse(Boolean.valueOf(response.getFirstHeader("serialized").getValue()));
             response.getEntity().getContent().close();
 
-            controller.stop(CONTAINER_SINGLE);
-            controller.start(CONTAINER_SINGLE);
+            stop(CONTAINER_SINGLE);
+            start(CONTAINER_SINGLE);
 
             response = client.execute(new HttpGet(url));
             Assert.assertEquals(HttpServletResponse.SC_OK, response.getStatusLine().getStatusCode());
@@ -110,7 +109,7 @@ public class NonHaWebSessionPersistenceTestCase {
             client.getConnectionManager().shutdown();
         }
 
-        deployer.undeploy(DEPLOYMENT_1);
-        controller.stop(CONTAINER_SINGLE);
+        undeploy(DEPLOYMENT_1);
+        stop(CONTAINER_SINGLE);
     }
 }
